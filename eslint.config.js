0dab1ad01ae89db53686b80875b62188ecd8731b
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2025-04-21 01:25:23 +0300

    Add stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
-import {defineConfig} from "eslint/config";
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import reactPlugin from "eslint-plugin-react";
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,ts,tsx}"],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactPlugin.configs.flat.recommended,
-            reactPlugin.configs.flat['jsx-runtime']
-        ],
-        languageOptions: { globals: globals.browser },
-    },
-]);
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      stylistic.configs.recommended,
+    ],
+    languageOptions: { globals: globals.browser },
+  },
+])
