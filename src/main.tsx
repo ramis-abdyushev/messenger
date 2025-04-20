88bec23a814ce6deca6f72e62fd969f52a9d18d4
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2025-04-20 21:42:32 +0300

    absolute import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {createRoot} from "react-dom/client";
 import {StrictMode} from "react";
-import App from "./App/App";
+import App from 'App/App.tsx';
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
